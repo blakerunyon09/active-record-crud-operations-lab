041a25f9ea2a0e04946433583bf613a63e077356
Blake Runyon <blakerunyon09@gmail.com>
2021-02-12 10:19:23 -0700

    Crushed.

---
+++ db/migrate/20210212152653_create_movie.rb
@@ -0,0 +1,9 @@
+class CreateMovie < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title, :director, :lead
+      t.integer :release_date
+      t.boolean :in_theaters
+    end
+  end
+end
